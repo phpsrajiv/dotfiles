636c4c3ef64e93ffd0e7e57016e4ab96283c7d20
Madhusudhan Srinivasa <madhums8@gmail.com>
2011-06-29 11:57:30 +0530

    create a backup

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+if [ ! -d ~/.dotfile_backup ]; then
+  mkdir ~/.dotfile_backup
+fi
+
 # Copy all the dotfiles
 for i in .*
 do
 	if [ "$i" != "." ] && [ "$i" != ".." ] && [ "$i" != ".git" ]; then
-		if ln -s $(pwd)/$i ~/test/$i; then
+	  if [ -e ~/$i ]; then
+			if ! ( cp ~/$i ~/.dotfile_backup/$i ) || ! ( rm ~/$i || unlink ~/$i ); then
+				echo "Failed on $i" > /dev/stderr
+				exit 1
+			fi
+		fi
+		if ln -s $(pwd)/$i ~/$i; then
 			echo "Linked: $i" > /dev/stderr
 		else
 			echo "Failed on $i" > /dev/stderr
