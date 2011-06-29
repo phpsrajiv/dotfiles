87fe0d2df599f3d7b30147aec2a4e939c3c8360c
Madhusudhan Srinivasa <madhums8@gmail.com>
2011-06-29 21:11:36 +0530

    change terminal fg color

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ gconftool-2 --set /apps/gnome-terminal/profiles/Default/visible_name -t str 'Def
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/alternate_screen_scroll -t bool false
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/palette -t str '#2E2E34343636:#CCCC00000000:#4E4E9A9A0606:#C4C4A0A00000:#34346565A4A4:#757550507B7B:#060698209A9A:#D3D3D7D7CFCF:#555557575353:#EFEF29292929:#8A8AE2E23434:#FCFCE9E94F4F:#72729F9FCFCF:#ADAD7F7FA8A8:#3434E2E2E2E2:#EEEEEEEEECEC'
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/background_color -t str '#222222222222'
-gconftool-2 --set /apps/gnome-terminal/profiles/Default/foreground_color -t str '#FFFFFFFFFFFF'
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/foreground_color -t str '#F3F3F3F3F3F3'
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/bold_color -t str '#000000000000'
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/allow_bold -t bool false
 
